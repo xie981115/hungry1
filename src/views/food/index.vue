9e11d5d798b264bf23aab6576af10753c074fa6c
xie981115 <2246967974@qq.com>
2020-10-04 19:06:39 +0800

    10.4.3

---
+++ src/views/food/index.vue
@@ -0,0 +1,30 @@
+<!--  -->
+<template>
+<div class=''>外卖32165</div>
+</template>
+
+<script>
+
+export default {
+components: {},
+data() {
+return {
+
+}
+},
+computed: {},
+watch: {},
+methods: {
+
+},
+created() {
+
+},
+mounted() {
+
+},
+}
+</script>
+<style scoped>
+
+</style>
